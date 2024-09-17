9836837e530aa1227040a30499d2165c2b791aed
daichi.s <daichi.sugawara@nijibox.co.jp>
2024-09-17 19:22:21 +0900

    feat: アプリタイトルセクションの追加

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+
+const page = () => {
+  return (
+    <>
+      <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
+        <div className="container px-8 mx-auto text-center flex flex-col items-center gap-4 max-w-[64rem]">
+          <Link
+            href={'/'}
+            className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm">
+            Xをフォローする
+          </Link>
+          <h1
+            className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl"
+          >
+            Post Writer
+          </h1>
+          <p className="text-muted-foreground sm:text-xl leading-normal max-w-[42rem]">
+            このアプリケーションはNext.js AppRouterで作られたものです。ユーザーは自由に投稿をポストすることができます。
+          </p>
+          <div className="space-x-4">
+            <Link
+              href={'/login'}
+              className={cn(buttonVariants({ size: 'lg' }))}
+            >
+              はじめる
+            </Link>
+            <Link
+              href={'/'}
+              className={cn(buttonVariants({ size: 'lg', variant: 'outline' }))}
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
