4017f9cf338039084501a9602c134b64f0d5f6c1
daichi.s <daichi.sugawara@nijibox.co.jp>
2024-09-26 13:55:51 +0900

    feat: featuresセクション追加

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -39,6 +39,17 @@ const page = () => {
           </div>
         </div>
       </section>
+
+      <section id="features" className="container py-8 md:py-12 lg:py-24 bg-slate-50">
+        <div className="text-center space-y-6 max-w-[58rem] mx-auto">
+          <h2 className="font-extrabold text-3xl md:text-6xl">
+            サービスの特徴
+          </h2>
+          <p className="text-muted-foreground sm:text-lg sm:leading-7">
+            このプロジェクトはモダンな技術スタックを使って作られたWebアプリケーションです。Next.js AppRouterやcontent layerを利用してマークダウン形式でブログ投稿ができます。
+          </p>
+        </div>
+      </section>
     </>
   );
 };
